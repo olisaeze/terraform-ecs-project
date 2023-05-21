8ac278c5e6430fc5e7d03e18a772bb16bcfb8afd
Olisa Eze <olisaeze@gmail.com>
2023-05-21 15:25:04 -0500

    RDS

--- rds.tf
+++ rds.tf
@@ -22,7 +22,7 @@ resource "aws_db_instance" "database_instance" {
   skip_final_snapshot    = true
   availability_zone      = data.aws_availability_zones.available_zones.names[1]
   identifier             = var.database_instance_identifier
-  snapshot_identifier    = data aws_db_snapshot.latest_db_snapshot.id
+  snapshot_identifier    = data.aws_db_snapshot.latest_db_snapshot.id
   db_subnet_group_name   = aws_db_subnet_group.database_subnet_group.name
   multi_az               = var.multi_az_deployment
   vpc_security_group_ids = [aws_security_group.database_security_group.id]
